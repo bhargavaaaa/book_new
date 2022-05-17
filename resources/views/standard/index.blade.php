71d8a1b8fa83092ce49959fb369513f55704c489
Bhargav Patel <bhargavchaniyara342@gmail.com>
2022-05-17 09:07:53 +0530

    added sweet alert changes

--- resources/views/standard/index.blade.php
+++ resources/views/standard/index.blade.php
@@ -18,19 +18,19 @@
     </div><!-- /.container-fluid -->
 </div>
 
-@if (session()->has("details_success"))
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-    {!! session()->get('details_success') !!}
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
-@endif
 
 <section class="content">
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
+                @if (session()->has("details_success"))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {!! session()->get('details_success') !!}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                @endif
                 <div class="card">
                     <div class="card-header">
                         {{-- <h3 class="card-title">{{ $moduleName ?? '' }} Details</h3> --}}
@@ -70,49 +70,51 @@
 @section('footer_script')
 
 <script>
-    // $.fn.dataTable.ext.errMode = 'throw';
-    $("#datatable").DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: {
-            "url": "{{ route('standard.getData') }}",
-            "dataType": "json",
-            "type": "GET",
-        },
-        columns: [{
-                data: 'DT_RowIndex',
-                orderable: false,
-                searchable: false
+    $(document).ready(function() {
+        // $.fn.dataTable.ext.errMode = 'throw';
+        $("#datatable").DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: {
+                "url": "{{ route('standard.getData') }}",
+                "dataType": "json",
+                "type": "GET",
             },
-            {
-                data: 'name',
-            },
-            {
-                data: 'action',
-                orderable: false,
-                searchable: false
-            },
-        ],
+            columns: [{
+                    data: 'DT_RowIndex',
+                    orderable: false,
+                    searchable: false
+                },
+                {
+                    data: 'name',
+                },
+                {
+                    data: 'action',
+                    orderable: false,
+                    searchable: false
+                },
+            ],
+        });
+    
+        $(document).on('click', '.delete', function(e) {
+            e.preventDefault();
+            var linkURL = $(this).attr("href");
+            console.log(linkURL);
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.value) {
+                    window.location.href = linkURL;
+                }
+            });
+        });
     });
-
-    // $(document).on('click', '.delete', function(e) {
-    //     e.preventDefault();
-    //     var linkURL = $(this).attr("href");
-    //     console.log(linkURL);
-    //     Swal.fire({
-    //         title: 'Are you sure?',
-    //         text: "You won't be able to revert this!",
-    //         icon: 'warning',
-    //         showCancelButton: true,
-    //         confirmButtonColor: '#3085d6',
-    //         cancelButtonColor: '#d33',
-    //         confirmButtonText: 'Yes, delete it!'
-    //     }).then((result) => {
-    //         if (result.value) {
-    //             window.location.href = linkURL;
-    //         }
-    //     });
-    // });
 </script>
 
 @endsection
--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -18,19 +18,19 @@
     </div><!-- /.container-fluid -->
 </div>
 
-@if (session()->has("details_success"))
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-    {!! session()->get('details_success') !!}
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
-@endif
 
 <section class="content">
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
+                @if (session()->has("details_success"))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {!! session()->get('details_success') !!}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                @endif
                 <div class="card">
                     <div class="card-header">
                         <div>
@@ -110,6 +110,25 @@
                 searchable: false
             },
         ],
+    });
+
+    $(document).on('click', '.delete', function(e) {
+        e.preventDefault();
+        var linkURL = $(this).attr("href");
+        console.log(linkURL);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.value) {
+                window.location.href = linkURL;
+            }
+        });
     });
 
     //  $(document).on('click', '#activate', function(e) {
